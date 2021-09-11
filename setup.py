be52dce80327e51d8aa57ed5a0815ac24bbdc0c5
samir char <samirchar@msn.com>
2021-09-11 11:10:00 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TBD',
+    author='Tomislav Galjanic, Abhishek Sinha, Christodoulos Constantinides, Samir Char',
+    license='MIT',
+)
